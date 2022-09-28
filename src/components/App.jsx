f31f62803578287374c41ba2139c32aeb36b37eb
Alex Lee <alexylee15@gmail.com>
2022-09-28 15:33:49 -0400

    creates answer choices randomly without repeats

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,12 @@ import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react
 import React, {useState, useEffect} from "react";
 import Home from './Home.jsx';
 import NCPier from './NcPier.jsx';
-import styled from 'styled-components';
+import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
+import styled, {createGlobalStyle} from 'styled-components';
+
+const bigStyle = createGlobalStyle`
+  box-sizing: border-box;
+`
 
 const AppDiv = styled.div`
   width: 100%;
@@ -13,12 +18,14 @@ const App = () => {
   const navigate = useNavigate();
 
   return (
+    <ScopedCssBaseline>
       <div className='App'>
         <Routes>
           <Route exact path='/' element={<Home navigate={navigate}/>}></Route>
           <Route exact path='/NCInshore' element={<NCPier navigate={navigate}/>}></Route>
         </Routes>
       </div>
+    </ScopedCssBaseline>
     )
 }
 
--- src/components/QuestionModal.jsx
+++ src/components/QuestionModal.jsx
@@ -1,6 +1,29 @@
 import React, {useState, useEffect, Component, useRef} from "react";
 import styled from 'styled-components';
-import { Modal, Box, Typography, Card, CardMedia, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@mui/material';
+import { Modal, Box, Typography, Card, CardMedia, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, createTheme, ThemeProvider, Button} from '@mui/material';
+
+
+const theme = createTheme({
+  overrides: {
+    // Style sheet name ⚛️
+    MuiCard: {
+      // Name of the rule
+      root: {
+        // Some CSS
+        overflow: 'scroll',
+      },
+    },
+  },
+});
+theme.typography.h3 = {
+  fontSize: '0.8rem',
+  '@media (min-width:600px)': {
+    fontSize: '1rem',
+  },
+  [theme.breakpoints.up('md')]: {
+    fontSize: '1rem',
+  },
+};
 
 
 const QuestionModal = ({fishData, setShowModal}) => {
@@ -16,6 +39,39 @@ const QuestionModal = ({fishData, setShowModal}) => {
     baglimitChoices.push(fishData[i].baglimit);
     sizelimitChoices.push(fishData[i].sizelimit);
   };
+  const toFindDuplicates = arry => arry.filter((item, index) => arry.indexOf(item) !== index);
+  const duplicateSize = toFindDuplicates(sizelimitChoices);
+
+  const replaceBagDuplicates = (arr) => {
+    let duplicates = toFindDuplicates(arr);
+    for (let i = 0; i < duplicates.length; i++) {
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[j] === duplicates[i]) {
+          arr[j] = ((Math.floor(Math.random() * 6) + 1) * 5) + ' / Day';
+          break;
+        }
+      }
+    }
+  }
+
+  const replaceSizeDuplicates = (arr) => {
+    let duplicates = toFindDuplicates(arr);
+    for (let i = 0; i < duplicates.length; i++) {
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[j] === duplicates[i]) {
+          arr[j] = (Math.floor(Math.random() * 9) + 8) + '" FL';
+          break;
+        }
+      }
+    }
+  }
+
+  while (toFindDuplicates(baglimitChoices).length > 0) {
+    replaceBagDuplicates(baglimitChoices);
+  }
+  while (toFindDuplicates(sizelimitChoices).length > 0) {
+    replaceSizeDuplicates(sizelimitChoices);
+  }
 
   console.log('CHOICES:', nameChoices, baglimitChoices, sizelimitChoices);
 
@@ -25,8 +81,8 @@ const QuestionModal = ({fishData, setShowModal}) => {
 
   return (
     <Modal open={true} onClose={() => {refreshPage()}}>
-      <Box position='absolute' top='10%' left='12.5%' sx={{display: 'flex', width: 3/4, height: 4/5, border: 1, borderColor: 'gray', borderRadius: 2, bgcolor: '#E5DFDB'}}>
-      <Box sx={{display: 'flex', flexDirection: 'column', alignContent: 'space-between', width: 5/11, height: 0.9, mt: 3, ml: 4}}>
+      <Box position='absolute' top='10%' left='12.5%' sx={{display: 'flex', overflow: 'scroll', width: 3/4, height: 4/5, border: 1, borderColor: 'gray', borderRadius: 2, bgcolor: '#E5DFDB'}}>
+      <Box sx={{display: 'flex', flexDirection: 'column', alignContent: 'space-between', width: 7/11, height: 0.9, mt: 3, ml: 4}}>
         <Card sx={{width: 0.9, height: 0.6}}>
           <CardMedia
             component='img'
@@ -39,49 +95,64 @@ const QuestionModal = ({fishData, setShowModal}) => {
 
           </Card>
       </Box>
-        <Card sx={{display: 'flex', flexDirection: 'space-evenly', width: 7/15, height: 0.9, mt: 3}}>
-          <Box sx={{display: 'flex', flexDirection: 'column', border: 1, m: 1}}>
-            <Box sx={{m: 2}}>
-              <FormControl>
-                <FormLabel id="demo-radio-buttons-group-label">Identify the fish!</FormLabel>
+        <Card sx={{display: 'flex', flexDirection: 'space-evenly', width: 4/11, height: 0.9, mt: 3, ml: -7}}>
+          <Box sx={{width: 0.7, display: 'flex', m: 2, flexDirection: 'column', alignContent: 'center'}}>
+            <Box sx={{height: 0.2, width: 0.95, m: 1, display: 'flex', flexWrap: 'wrap', flexGrow: 1, flexShrink: 1,  flex: 'auto'}}>
+              <FormControl >
+                  <FormLabel id="demo-radio-buttons-group-label">
+                    Identify the fish
+                  </FormLabel>
                 <RadioGroup
                   aria-labelledby="demo-radio-buttons-group-label"
                   name="radio-buttons-group"
+                  sx={{height: 0.5}}
                   >
-                  {nameChoices.map((option, index) => {
-                    return <FormControlLabel value={option} control={<Radio />} label={option}/>
+                  {nameChoices.map((option, key) => {
+                    return <FormControlLabel value={option} key={key}  control={<Radio/>} label={option}/>
                   })}
                 </RadioGroup>
               </FormControl>
             </Box>
-            <Box sx={{m: 2}}>
+            <Box sx={{height: 0.2, width: 0.95, m: 1, display: 'flex', flexWrap: 'wrap', flexGrow: 1, flexShrink: 1,  flex: 'auto'}}>
               <FormControl>
-                <FormLabel id="demo-radio-buttons-group-label">Identify the fish!</FormLabel>
+                <FormLabel id="demo-radio-buttons-group-label">
+                  What is the current size limit?
+                </FormLabel>
                 <RadioGroup
                   aria-labelledby="demo-radio-buttons-group-label"
                   name="radio-buttons-group"
+                  sx={{height: 0.5, width: 0.8}}
                   >
-                  {nameChoices.map((option, index) => {
-                    return <FormControlLabel value={option} control={<Radio />} label={option}/>
+                  {sizelimitChoices.map((option, key) => {
+                    return <FormControlLabel value={option} key={key} control={<Radio />} label={option}/>
                   })}
                 </RadioGroup>
               </FormControl>
             </Box>
-            <Box sx={{m: 2}}>
+            <Box sx={{height: 0.2, width: 0.95, m: 1, display: 'flex', flexWrap: 'wrap', flexGrow: 1, flexShrink: 1,  flex: 'auto'}}>
               <FormControl>
-                <FormLabel id="demo-radio-buttons-group-label">Identify the fish!</FormLabel>
+                  <FormLabel id="demo-radio-buttons-group-label">
+                    What is the current bag limit per person?
+                  </FormLabel>
                 <RadioGroup
                   aria-labelledby="demo-radio-buttons-group-label"
                   name="radio-buttons-group"
+                  sx={{height: 0.5}}
                   >
-                  {nameChoices.map((option, index) => {
-                    return <FormControlLabel value={option} control={<Radio />} label={option}/>
+                  {baglimitChoices.map((option, key) => {
+                    return <FormControlLabel value={option} key={key} control={<Radio />} label={option}/>
                   })}
                 </RadioGroup>
               </FormControl>
+            </Box>
+            <Box>
+              <Button variant='contained' size='medium' sx={{mb: 3}}>Submit!</Button>
+              <Typography>*FL: Fork Length  &nbsp; &nbsp; &nbsp;  *TL: Tail Length</Typography>
             </Box>
           </Box>
-          <Box sx={{border: 1, m: 1}}></Box>
+          <Box sx={{border: 1, m: 1}}>
+            {/* answer stuff here */}
+          </Box>
         </Card>
       </Box>
     </Modal>
